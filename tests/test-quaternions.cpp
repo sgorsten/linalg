6964e2b5e126374cb2286abde28f62d2451374e0
Sterling Orsten <sgorsten@gmail.com>
2018-10-20 21:21:17 -0700

    Slight change to tests.

--- tests/test-quaternions.cpp
+++ tests/test-quaternions.cpp
@@ -91,10 +91,7 @@ TEST_CASE_TEMPLATE("qxdir(q) == qvq* where v={1,0,0,0}",  T, int, float, double)
     for(int i=0; i<reps; ++i)
     {
         const quat<T> q = rng;
-        const vec3<T> qx = qxdir(q), qx_ref = (q * quat<T>{1,0,0,0} * conjugate(q)).xyz();
-        CHECK(qx.x == doctest::Approx(qx_ref.x));
-        CHECK(qx.y == doctest::Approx(qx_ref.y));
-        CHECK(qx.z == doctest::Approx(qx_ref.z));
+        check_approx_equal(quat<T>{qxdir(q),0}, q * quat<T>{1,0,0,0} * conjugate(q));
     }
 }
 
@@ -104,10 +101,7 @@ TEST_CASE_TEMPLATE("qzdir(q) == qvq* where v={0,1,0,0}",  T, int, float, double)
     for(int i=0; i<reps; ++i)
     {
         const quat<T> q = rng;
-        const vec3<T> qy = qydir(q), qy_ref = (q * quat<T>{0,1,0,0} * conjugate(q)).xyz();
-        CHECK(qy.x == doctest::Approx(qy_ref.x));
-        CHECK(qy.y == doctest::Approx(qy_ref.y));
-        CHECK(qy.z == doctest::Approx(qy_ref.z));
+        check_approx_equal(quat<T>{qydir(q),0}, q * quat<T>{0,1,0,0} * conjugate(q));
     }
 }
 
@@ -117,10 +111,7 @@ TEST_CASE_TEMPLATE("qzdir(q) == qvq* where v={0,0,1,0}",  T, int, float, double)
     for(int i=0; i<reps; ++i)
     {
         const quat<T> q = rng;
-        const vec3<T> qz = qzdir(q), qz_ref = (q * quat<T>{0,0,1,0} * conjugate(q)).xyz();
-        CHECK(qz.x == doctest::Approx(qz_ref.x));
-        CHECK(qz.y == doctest::Approx(qz_ref.y));
-        CHECK(qz.z == doctest::Approx(qz_ref.z));
+        check_approx_equal(quat<T>{qzdir(q),0}, q * quat<T>{0,0,1,0} * conjugate(q));
     }
 }
 
@@ -132,10 +123,9 @@ TEST_CASE_TEMPLATE("qrot(q,v) == qvq*",  T, int, float, double)
         const quat<T> q = rng;
         for(int j=0; j<reps; ++j)
         {
-            const vec3<T> v = rng, qr = qrot(q,v), qr_ref = (q * quat<T>{v,0} * conjugate(q)).xyz();
-            CHECK(qr.x == doctest::Approx(qr_ref.x));
-            CHECK(qr.y == doctest::Approx(qr_ref.y));
-            CHECK(qr.z == doctest::Approx(qr_ref.z));
+            const vec3<T> v = rng;
+            check_approx_equal(quat<T>{qrot(q,v),0}, q * quat<T>{v,0} * conjugate(q));
+            check_approx_equal(qrot(q,v), qmat(q)*v);
         }
     }
 }
